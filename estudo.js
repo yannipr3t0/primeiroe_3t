aa04090789700875480f5df57847c0c475ae0ecd
yannipr3t0 <110101956+yannipr3t0@users.noreply.github.com>
2022-10-19 09:07:04 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -15,9 +15,9 @@ function escreva(){
 }
 
 function minhaTabuada(){
-    for(let i=1; i <= 10; i++){
+    for(let i=1; i = 10; i++){
         document.write("Tabuada do " + i + "<br>");
-        for(let j = 1; j <= 10; j++){
+        for(let j = 1; j = 10; j++){
             document.write(i + " x " + j + " = " + (j*i)+"<br>");
         }
         document.write("<br>");
@@ -25,7 +25,43 @@ function minhaTabuada(){
 }
 
 function quadrado(){
-    for(let i = 2; <= 1000; i++){
+    for(let i = 2;i = 1000; i++){
         document.write("O quadrado de " + i + " é " + (i*i) + "<br>");
     }
+}
+
+function calcula(){
+    let val = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("mes").value;
+    
+    let res = val * (1+(j/100))
+    
+    document.write("Resultado: "+res);
+}
+
+function somaNota(){
+    let n1 = document.getElementById("t1").value;
+    let n2 = document.getElementById("t2").value;
+    let n3 = document.getElementById("t3").value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    document.getElementById("resultado").innerHTML = "Soma: " + r;
+}
+
+function somaMedia(){
+    let n1 = document.getElementById("t1").value;
+    let n2 = document.getElementById("t2").value;
+    let n3 = document.getElementById("t3").value;
+    let d = 3;
+    let r = Number(n1) + Number(n2) + Number(n3)/d;
+    document.getElementById("resultado").innerHTML = "Média: " + r;
+}
+
+function somaFalta(){
+    let n1 = document.getElementById("t1").value;
+    let n2 = document.getElementById("t2").value;
+    let n3 = document.getElementById("t3").value;
+    let d = 180;
+    let r = Number(n1) + Number(n2) + Number(n3) - d;
+    document.getElementById("resultado").innerHTML = "Falta: " + r;
 }
